e4c61a6e69a7ceea98632bcc230dd252a6a20177
Antoine McAbee <antoinemcabee1211@gmail.com>
2020-05-24 02:14:19 -0500

    Updates

---
+++ precache-manifest.d57841dc79009c474bffb9093f45798a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88d81ae7ef5ac339c7328dbc15d5b01",
+    "url": "/cgwaco/index.html"
+  },
+  {
+    "revision": "946a4615f49a70e6afb1",
+    "url": "/cgwaco/static/css/main.9e1e39b5.chunk.css"
+  },
+  {
+    "revision": "b90b7619a394edd8b4d6",
+    "url": "/cgwaco/static/js/2.de700074.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cgwaco/static/js/2.de700074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946a4615f49a70e6afb1",
+    "url": "/cgwaco/static/js/main.f4075478.chunk.js"
+  },
+  {
+    "revision": "6cf6f4395c815d03f7aa",
+    "url": "/cgwaco/static/js/runtime-main.1664a574.js"
+  },
+  {
+    "revision": "64bd8c67cff0f17df1566a3484e415e8",
+    "url": "/cgwaco/static/media/CG_Full+Logo.64bd8c67.png"
+  },
+  {
+    "revision": "4939ffc250a144b3b080a4c4ab74d164",
+    "url": "/cgwaco/static/media/beans2.4939ffc2.jpg"
+  },
+  {
+    "revision": "04fd4d69da45cbec06b822e949696ae2",
+    "url": "/cgwaco/static/media/gounds1.04fd4d69.jpg"
+  }
+]);
