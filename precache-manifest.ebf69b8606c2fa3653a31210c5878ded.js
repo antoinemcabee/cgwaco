cc81336a0b3d80d4508e8d1a77f1793d42b2f61f
Antoine McAbee <antoinemcabee1211@gmail.com>
2020-05-24 01:28:53 -0500

    Updates

---
+++ precache-manifest.ebf69b8606c2fa3653a31210c5878ded.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e224e1e002f6d92f23fc7956b46c8422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf4f1b53241705523dee",
+    "url": "/static/css/main.71a664d9.chunk.css"
+  },
+  {
+    "revision": "b90b7619a394edd8b4d6",
+    "url": "/static/js/2.de700074.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de700074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4f1b53241705523dee",
+    "url": "/static/js/main.b69a7917.chunk.js"
+  },
+  {
+    "revision": "be2b74b52f2f90603dbb",
+    "url": "/static/js/runtime-main.8ef77653.js"
+  },
+  {
+    "revision": "64bd8c67cff0f17df1566a3484e415e8",
+    "url": "/static/media/CG_Full+Logo.64bd8c67.png"
+  },
+  {
+    "revision": "4939ffc250a144b3b080a4c4ab74d164",
+    "url": "/static/media/beans2.4939ffc2.jpg"
+  },
+  {
+    "revision": "04fd4d69da45cbec06b822e949696ae2",
+    "url": "/static/media/gounds1.04fd4d69.jpg"
+  }
+]);
