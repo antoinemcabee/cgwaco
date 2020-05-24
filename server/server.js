ef6e97cdc6a9f8b3354af513d4e9613738fd5e7d
Antoine McAbee <antoinemcabee1211@gmail.com>
2020-05-24 01:04:44 -0500

    deleted the proxySetup file which had an uninstall dependency

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
+const indexPath = path.join(__dirname, '..', 'src');
 
-console.log(publicPath);
+console.log(`Public Path: ${publicPath}`);
+console.log(`Index Path: ${indexPath}`);
 
 app.use(express.static(publicPath));
+app.use(express.static(indexPath));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
